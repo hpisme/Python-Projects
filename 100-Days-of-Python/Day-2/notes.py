ec3fd9d36de050ad77b1ed35e3493be4ee16786c
hpisme <heather.pemberton2@gmail.com>
2021-05-20 19:46:29 -0700

    Update notes.py

--- 100-Days-of-Python/Day-2/notes.py
+++ 100-Days-of-Python/Day-2/notes.py
@@ -1,1 +1,33 @@
 
+"""Formatting strings or F Strings"""
+
+#An F-string along with {} can be used as a placeholder in strings
+greeting = 'Hello'
+print(f'{greeting}, how are you today?')
+
+#The .format method can also be used
+print("Hello {}, how are you today?".format('John'))
+
+
+"""Mathematical Operations"""
+
+#Addition
+1 + 1
+
+#Subtraction
+1 - 1
+
+#Division
+1 / 2
+
+#Floor division (rounds down)
+3 // 2
+
+#Multiplication
+1 * 1
+
+#Exponentiation
+1 ** 1 
+
+#Modulus (returns remainder)
+3 % 2
