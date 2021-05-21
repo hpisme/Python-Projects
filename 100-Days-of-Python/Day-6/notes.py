4622cf19f3bd9debf13623a88fe584081ebcb169
hpisme <heather.pemberton2@gmail.com>
2021-05-20 20:38:07 -0700

    Update notes.py

--- 100-Days-of-Python/Day-6/notes.py
+++ 100-Days-of-Python/Day-6/notes.py
@@ -6,3 +6,10 @@ def myfunction(x): #functions can have parameters
   print(1 + x)
 
 myfunction(5) #calling the function with an argument
+
+"""Indentation"""
+
+#Better to use spaces vs tabs and spaces in Python are important
+x = 2
+if x > 1:
+  print('Hello!') #two spaces to indent the code after the : 
