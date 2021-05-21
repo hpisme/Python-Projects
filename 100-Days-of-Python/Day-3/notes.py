749e4a517966010a47d884e8a0db2110c774195e
hpisme <heather.pemberton2@gmail.com>
2021-05-20 19:53:35 -0700

    Create notes.py

---
+++ 100-Days-of-Python/Day-3/notes.py
@@ -0,0 +1,20 @@
+"""Conditionals / Flow Control"""
+
+#If loops
+if 2 > 1:
+  print('2 is greater than 1')
+  
+#Adding an else statement
+if 3 < 2:
+  print('This won\'t print')
+else:
+  print('This will print!')
+  
+ #Adding an elif statement
+x = 0
+if x == 1:
+  print('This will print if x is 1.')
+elif x == 2:
+  print('This will print if x is 2.')
+else:
+  print('This will print if x is not 1 or 2.')
