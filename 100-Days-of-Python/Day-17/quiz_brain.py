152d81c27dfecd87949bcb92057f59ad6ce4ac70
hpisme <heather.pemberton2@gmail.com>
2021-06-05 16:16:28 -0700

    Update quiz_brain.py

--- 100-Days-of-Python/Day-17/quiz_brain.py
+++ 100-Days-of-Python/Day-17/quiz_brain.py
@@ -1,18 +1,26 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrain
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.questions_list = q_list
+        self.score = 0
 
-question_bank = []
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+    def still_has_questions(self):
+        return self.question_number < len(self.questions_list)
 
-new_quiz = QuizBrain(question_bank)
+    def next_question(self):
+        current_question = self.questions_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
 
-while new_quiz.still_has_questions():
-    new_quiz.next_question()
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score} / {self.question_number}")
+        print('\n')
 
-print("You've completed the quiz.")
-print(f"Your final score is {new_quiz.score} / {len(question_bank)}")
+
